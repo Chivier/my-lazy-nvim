661acce088c31b3978e12da1c93e952f8c57b3fb
Chivier Humber <chivier.humber@outlook.com>
2023-03-13 15:25:13 +0800

    update better auto complete with tab

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -213,15 +213,15 @@ return {
     },
   },
 
-  -- Use <tab> for completion and snippets (supertab)
-  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  -- then: setup supertab in cmp
+  -- -- Use <tab> for completion and snippets (supertab)
+  -- -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   keys = function()
+  --     return {}
+  --   end,
+  -- },
+  -- -- then: setup supertab in cmp
   -- {
   --   "hrsh7th/nvim-cmp",
   --   dependencies = {
